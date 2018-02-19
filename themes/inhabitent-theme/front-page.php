834f27271c3c7a8fdd1b9092f8f9658792b03d1a
chiwwy <ermiljordan64@gmail.com>
2018-02-19 12:52:37 +0900

    add journal in home

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -16,6 +16,24 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <!-- section for shop stuff -->
-
+<section>
+	<h2>Shop Stuff</h2>
+	<div>
+	<?php $terms = get_terms('product_type');
+		foreach ( $terms as $term ) {
+	?>
+	<?php 
+		$url = get_term_link($term->slug, 'product_type');
+	?>
+	<div>
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/product-type-icons/<?php echo $term->slug ?>.svg">
+		<p><?php echo $term->description ?></p>
+		<p><a href="<?php echo $url ?>"><?php echo $term->name.''; ?></a></p>
+	</div>
+		<?php
+		}
+		?>
+	</div>
+</section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
